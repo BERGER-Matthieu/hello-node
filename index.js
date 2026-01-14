76fce276a0c5a98aab8c862117d1929b8734fb78
BERGER-Matthieu <matthieu.berger.27430@gmail.com>
2026-01-14 16:10:15 +0100

    ports 80 -> 8080

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.get("/random-teto-music", (req, res) => {
 })
 
 
-app.listen(80, () => {
-    console.log("server is running on 127.0.0.1:3000")
+app.listen(8080, () => {
+    console.log("server is running on 127.0.0.1:8080")
 })
 
 // () => {}
